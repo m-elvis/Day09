d9143408df28aff8f385e41f3b3a0961e1211e58
m-elvis <elvis.mahovic@gmail.com>
2021-03-16 22:46:43 +0100

    varresult

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,10 +1,12 @@
 function functionElse(num) {
+    var result;
     if(num <= 5) {
-        return "5 or smaller";
+        result = "5 or smaller";
     } else {
-        return "Bigger than 5";
+        result = "Bigger than 5";
     }
-}
+    return result;
+   }
 console.log(functionElse(4));
 console.log(functionElse(5));
 console.log(functionElse(6));
